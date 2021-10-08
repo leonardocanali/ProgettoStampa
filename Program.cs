a5e33f4da89c03c036cc08cf48ca7aa799f7a3aa
Jacopocasanova <91311607+Jacopocasanova@users.noreply.github.com>
2021-10-08 09:28:40 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleStampa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ciao!");
+            Console.WriteLine("No vinco io Hi");
             Console.ReadKey();
         }
     }
